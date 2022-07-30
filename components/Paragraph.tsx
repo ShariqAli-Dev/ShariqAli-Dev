da5492bbd0126e47df9adda9acd98b0733cd95e1
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-07-30 15:16:29 -0400

    added auto break hypens into project paragraph

--- components/Paragraph.tsx
+++ components/Paragraph.tsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 const Paragraph = styled.p`
   text-align: justify;
   text-indent: 1em;
+  overflow-wrap: break-word;
+  hyphens: auto;
 `;
 
 export default Paragraph;
