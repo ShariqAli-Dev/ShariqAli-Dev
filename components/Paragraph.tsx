45475bc8ed21a28bd178c8ae51a5b46d4d97e448
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-07-30 15:20:41 -0400

    removed auto-wrap  break, spacing was fine initially

--- components/Paragraph.tsx
+++ components/Paragraph.tsx
@@ -3,8 +3,6 @@ import styled from '@emotion/styled';
 const Paragraph = styled.p`
   text-align: justify;
   text-indent: 1em;
-  overflow-wrap: break-word;
-  hyphens: auto;
 `;
 
 export default Paragraph;
